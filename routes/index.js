78b99a42cdd7d308d575df9da19b86a5100af359
Terri Jaskolka <jas20003@byui.edu>
2022-06-05 00:41:48 -0400

    working on google login

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/inventory', require('./inventory'));
 router.use('/', require('./inventory'));
+
 // router.use('/user', require('./user'));
+// router.use('/', require('./user'));
 
 module.exports = router;
